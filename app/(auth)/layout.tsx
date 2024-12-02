9387899711afbb3a1625505cb565b21bef6023e9
Aaron Choi <aaronsmc@alumni.stanford.edu>
2024-12-02 22:55:21 +0900

    update site metadta: change title

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google"
 import '../globals.css'
 
 export const metadata = {
-    title: 'Threads',
-    description: 'A Next.js 13 Meta Threads Application'
+    title: 'You Might Fall In Love Today',
+    description: 'A platform to share and express moments of love in our daily lives'
 
 }
 
